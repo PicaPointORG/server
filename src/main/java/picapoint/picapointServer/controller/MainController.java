5bdfa5258351d518f1e5b292417cce9efdf386ab
Doqso <p3lopubico@gmail.com>
2023-01-26 21:12:18 +0100

    metodos getEmpresas() de prueba para ruben

--- src/main/java/picapoint/picapointServer/controller/MainController.java
+++ src/main/java/picapoint/picapointServer/controller/MainController.java
@@ -1,23 +1,43 @@
 package picapoint.picapointServer.controller;
 
+import jakarta.servlet.http.Cookie;
+import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import picapoint.picapointServer.entities.Empresa;
 import picapoint.picapointServer.service.EmpresaService;
 
 import java.util.List;
+import java.util.Map;
 
 @RestController
 @RequestMapping("/")
 public class MainController {
     EmpresaService empresaService;
+
     public MainController(EmpresaService empresaService) {
         this.empresaService = empresaService;
     }
 
-    @GetMapping
-    public List<Empresa> getAllEmpresas() {
+    @GetMapping("/login")
+    public void login() {
+    }
+
+    @GetMapping("/registro")
+    public void registro() {
+
+    }
+
+    @GetMapping("/empresas")
+    public List<Empresa> getEmpresas(HttpServletResponse response) {
+        // create a cookie
+        Cookie cookie = new Cookie("username", "Jovan");
+
+        //add cookie to response
+        response.addCookie(cookie);
         return empresaService.getAll();
     }
 }

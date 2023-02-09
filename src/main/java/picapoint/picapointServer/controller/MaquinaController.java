feceecfe44e177775f33ccf7b9b15e31869f6898
Doqso <p3lopubico@gmail.com>
2023-02-09 16:21:23 +0100

    el controller para las maquinas implementada

---
+++ src/main/java/picapoint/picapointServer/controller/MaquinaController.java
@@ -0,0 +1,26 @@
+package picapoint.picapointServer.controller;
+
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+import picapoint.picapointServer.service.MaquinaService;
+
+import java.util.Map;
+
+@RestController
+@RequestMapping("/maquina")
+public class MaquinaController {
+    MaquinaService maquinaService;
+
+    public MaquinaController(MaquinaService maquinaService) {
+        this.maquinaService = maquinaService;
+    }
+
+    @PostMapping("/update")
+    public void updateMaquinaStock(@RequestParam Map<String, String> request) {
+        String mac = request.get("mac");
+        String idProducto = request.get("idProducto");
+        maquinaService.updateMaquinaStock(mac, Long.parseLong(idProducto));
+    }
+}
